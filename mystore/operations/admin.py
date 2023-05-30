69c978a1a481e23508c4f4a2428f10645697bdc4
GAMING <Xpom>
2023-05-30 10:05:05 +0300

    Added DB + JWT

--- mystore/operations/admin.py
+++ mystore/operations/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Cart, Product, Product_photo
+
+# admin.site.register(Cart)
+admin.site.register(Product)
+admin.site.register(Product_photo)
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    filter_horizontal = ['product_id']
--- mystore/operations/models.py
+++ mystore/operations/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Cart(models.Model):
+    customer_id = models.ForeignKey(User, on_delete=models.PROTECT)
+    product_id = models.ManyToManyField('Product')
+
+    def __str__(self):
+        return f'Cart - {User.get_username(self.customer_id)}'
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=1024, null=True)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.name} - {self.price}руб'
+
+
+class Product_photo(models.Model):
+    url = models.URLField(max_length=200)
+    product_id = models.ForeignKey('Product', on_delete=models.PROTECT)
+
+
